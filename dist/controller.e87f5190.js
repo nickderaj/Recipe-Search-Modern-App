d562a76e8df13b58f748365e58730576c80229e8
Nick De Raj <nickderaj@gmail.com>
2021-07-04 11:21:09 +0800

    Rendering recipe to HTML

--- dist/controller.e87f5190.js
+++ dist/controller.e87f5190.js
@@ -891,7 +891,7 @@ var timeout = function timeout(s) {
 
 var showRecipe = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    var res, data, recipe;
+    var res, data, recipe, markup;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -916,8 +916,6 @@ var showRecipe = /*#__PURE__*/function () {
             throw new Error("".concat(data.message, " (").concat(res.status, ")"));
 
           case 9:
-            console.log(res);
-            console.log(data);
             recipe = data.data.recipe;
             recipe = {
               id: recipe.id,
@@ -929,7 +927,10 @@ var showRecipe = /*#__PURE__*/function () {
               cookingTime: recipe.cooking_time,
               ingredients: recipe.ingredients
             };
-            console.log(recipe);
+            console.log(recipe); // Rendering recipe:
+
+            markup = "\n    <figure class=\"recipe__fig\">\n    <img src=\"".concat(recipe.image, "\" alt=\"").concat(recipe.title, "\" class=\"recipe__img\" />\n    <h1 class=\"").concat(recipe.title, "\">\n      <span>Pasta with tomato cream sauce</span>\n    </h1>\n  </figure>\n\n  <div class=\"recipe__details\">\n    <div class=\"recipe__info\">\n      <svg class=\"recipe__info-icon\">\n        <use href=\"src/img/icons.svg#icon-clock\"></use>\n      </svg>\n      <span class=\"recipe__info-data recipe__info-data--minutes\">").concat(recipe.cookingTime, "</span>\n      <span class=\"recipe__info-text\">minutes</span>\n    </div>\n    <div class=\"recipe__info\">\n      <svg class=\"recipe__info-icon\">\n        <use href=\"src/img/icons.svg#icon-users\"></use>\n      </svg>\n      <span class=\"recipe__info-data recipe__info-data--people\">").concat(recipe.servings, "</span>\n      <span class=\"recipe__info-text\">servings</span>\n\n      <div class=\"recipe__info-buttons\">\n        <button class=\"btn--tiny btn--increase-servings\">\n          <svg>\n            <use href=\"src/img/icons.svg#icon-minus-circle\"></use>\n          </svg>\n        </button>\n        <button class=\"btn--tiny btn--increase-servings\">\n          <svg>\n            <use href=\"src/img/icons.svg#icon-plus-circle\"></use>\n          </svg>\n        </button>\n      </div>\n    </div>\n\n    <div class=\"recipe__user-generated\">\n      <svg>\n        <use href=\"src/img/icons.svg#icon-user\"></use>\n      </svg>\n    </div>\n    <button class=\"btn--round\">\n      <svg class=\"\">\n        <use href=\"src/img/icons.svg#icon-bookmark-fill\"></use>\n      </svg>\n    </button>\n  </div>\n\n  <div class=\"recipe__ingredients\">\n    <h2 class=\"heading--2\">Recipe ingredients</h2>\n    <ul class=\"recipe__ingredient-list\">\n      <li class=\"recipe__ingredient\">\n        <svg class=\"recipe__icon\">\n          <use href=\"src/img/icons.svg#icon-check\"></use>\n        </svg>\n        <div class=\"recipe__quantity\">1000</div>\n        <div class=\"recipe__description\">\n          <span class=\"recipe__unit\">g</span>\n          pasta\n        </div>\n      </li>\n\n      <li class=\"recipe__ingredient\">\n        <svg class=\"recipe__icon\">\n          <use href=\"src/img/icons.svg#icon-check\"></use>\n        </svg>\n        <div class=\"recipe__quantity\">0.5</div>\n        <div class=\"recipe__description\">\n          <span class=\"recipe__unit\">cup</span>\n          ricotta cheese\n        </div>\n      </li>\n    </ul>\n  </div>\n\n  <div class=\"recipe__directions\">\n    <h2 class=\"heading--2\">How to cook it</h2>\n    <p class=\"recipe__directions-text\">\n      This recipe was carefully designed and tested by\n      <span class=\"recipe__publisher\">").concat(recipe.publisher, "</span>. Please check out\n      directions at their website.\n    </p>\n    <a\n      class=\"btn--small recipe__btn\"\n      href=\"").concat(recipe.sourceUrl, "\"\n      target=\"_blank\"\n    >\n      <span>Directions</span>\n      <svg class=\"search__icon\">\n        <use href=\"src/img/icons.svg#icon-arrow-right\"></use>\n      </svg>\n    </a>\n  </div>");
+            recipeContainer.insertAdjacentHTML('afterbegin', markup);
             _context.next = 19;
             break;
 
